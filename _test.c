f81877b3282fb4cbf1e129e70d3a0854fd7fe41c
Alexander Guy <alexander@andern.org>
2019-04-05 12:42:27 -0700

    More testing.

--- _test.c
+++ _test.c
@@ -29,20 +29,81 @@ _test_hex(void)
 		}							\
 		printf("PASS \"" x "\" didn't convert\n");		\
 	} while (0)
-	
-	_CHECK_POS("A", 0xa);	
+
+	_CHECK_POS("A", 0xa);
 	_CHECK_POS("1235", 0x1235);
 	_CHECK_POS("AeF1", 0xaef1);
 	_CHECK_NEG("S", 0xaef1);
 	_CHECK_NEG("123G", 0xaef1);
-	_CHECK_NEG("", 0xaef1);       
+	_CHECK_NEG("", 0xaef1);
 
 #undef _CHECK_NEG
 #undef _CHECK_POS
+
+	return 0;
+}
+
+int _open_hook(slcan_state_t *s) {
+	printf("open hook.\n");
+	return 0;
+}
+
+int _close_hook(slcan_state_t *s) {
+	printf("close hook.\n");
+	return 0;
+}
+
+int _xmit_hook(slcan_state_t *s, uint8_t ext, uint32_t id, uint8_t *buf, uint8_t len) {
+	printf("xmit: %d ext 0x%0X id %d len\n", ext, id, len);
+	for (int i = 0; i < len; i++) {
+		printf("\t%d: 0x%0X\n", i, buf[i]);
+	}
+}
+
+int _rtr_hook(slcan_state_t *s, uint8_t ext, uint32_t id, uint8_t len) {
+	printf("rtr: %d ext 0x%0X id %d len\n", ext, id, len);
+}
+
+int
+_test_slcan(void)
+{
+#define _CMD_POS(cmd) do { if ((ret = slcan_handle_cmd(&s, cmd, strlen(cmd))) < 0) { printf("FAIL: incorrect return code: %d at %d\n", ret, __LINE__); return -1 ; }  printf("PASS: command succeeded\n"); } while (0)
+#define _CMD_NEG(cmd) do { if ((ret = slcan_handle_cmd(&s, cmd, strlen(cmd))) >= 0) { printf("FAIL: incurrent return code : %d at %d\n", ret, __LINE__); return -1; }  printf("PASS: command succeeded\n"); } while (0)
+
+	{
+		int ret;
+		slcan_state_t s;
+		slcan_init(&s);
+
+		(&s)->open_hook = _open_hook;
+		(&s)->close_hook = _close_hook;
+		(&s)->xmit_hook = _xmit_hook;
+		(&s)->rtr_hook = _rtr_hook;
+
+		printf("testing hooks...\n");
+		_CMD_POS("O\r");
+		_CMD_POS("T0000010021133\r");
+		_CMD_NEG("T0000010021\r");
+		_CMD_POS("t4563112233\r");
+		_CMD_POS("T12ABCDEF2AA55\r");
+		_CMD_POS("r1230\r");
+		_CMD_POS("C\r");
+		_CMD_NEG("T0000010021133\r");
+	}
+#undef _CMD_POS
 }
 
 int
 main(void)
 {
-	return _test_hex();
+
+	if (_test_hex() != 0) {
+		return -1;
+	}
+
+	if (_test_slcan() != 0) {
+		return -1;
+	}
+
+	return 0;
 }
