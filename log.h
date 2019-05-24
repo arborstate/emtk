a464deccf01d54c2acb40134916d95ce05ce87d4
Alexander Guy <alexander@andern.org>
2019-05-24 15:11:15 -0700

    Normalize this with other code.

--- log.h
+++ log.h
@@ -3,10 +3,9 @@
 
 void _log(const char *level, const char *fmt, ...);
 
-#define LOG_DEBUG(...) _log("DEBUG", __VA_ARGS__)
-#define LOG_INFO(...) _log("INFO", __VA_ARGS__)
-#define LOG_WARN(...) _log("WARN", __VA_ARGS__)
-#define LOG_ERROR(...) _log("ERROR", __VA_ARGS__)
-
+#define LOG_DEBUG(fmt, ...) do { _log("DEBUG", fmt, ##__VA_ARGS__); } while (0)
+#define LOG_INFO(fmt, ...) do { _log("INFO", fmt, ##__VA_ARGS__); } while (0)
+#define LOG_WARN(fmt, ...) do { _log("WARN", fmt, ##__VA_ARGS__); } while (0)
+#define LOG_ERROR(fmt, ...) do { _log("ERROR", fmt, ##__VA_ARGS__); } while (0)
 
 #endif /* __LOG_H__ */
