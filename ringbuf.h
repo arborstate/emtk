26a7068874e5dfe088557ff91b8dc6473e7cf530
Alexander Guy <alexander@andern.org>
2019-07-15 11:21:52 -0700

    Ringbuf init.

--- ringbuf.h
+++ ringbuf.h
@@ -2,15 +2,19 @@
 #define __RINGBUF_H__
 #include <stddef.h>
 
+#define RINGBUF_TYPE(type, size) struct ringbuf_ ## type ## _size_ ## size
+
 #define RINGBUF_DEF(type, size) \
-	struct ringbuf_ ## type ## _size_ ## size {	\
+	RINGBUF_TYPE(type, size) {			\
 		type buf[size];				\
 		size_t wpos;				\
 		size_t rpos;				\
 	}
 
+
+
 #define RINGBUF_INST(name, type, size)				\
-	volatile struct ringbuf_ ## type ## _size_ ## size name
+	volatile RINGBUF_TYPE(type, size) name
 
 #define RINGBUF_DIST(name) \
 	(((name.rpos + sizeof(name.buf)) - name.wpos) % sizeof(name.buf))
@@ -18,4 +22,5 @@
 #define RINGBUF_INC(name, field) \
 	name.field = (name.field + 1) % sizeof(name.buf)
 
+#define RINGBUF_INIT(name) do { memset((void *)&name, 0, sizeof(name)); } while(0)
 #endif /* __RINGBUF_H__ */
